11d24e4fc02be100b957e8a699a6b0c5be81c637
Dima Bart <dima.bart01@gmail.com>
2024-01-29 20:31:02 -0500

    Add compute budget definitions.

---
+++ CodeServices/Sources/CodeServices/Solana/Instructions/Programs/ComputeBudgetProgram.SetComputeUnitPrice.swift
@@ -0,0 +1,53 @@
+//
+//  ComputeBudgetProgram.SetComputeUnitPrice.swift
+//  CodeServices
+//
+//  Created by Dima Bart.
+//  Copyright © 2021 Code Inc. All rights reserved.
+//
+
+import Foundation
+
+extension ComputeBudgetProgram {
+    public struct SetComputeUnitPrice: Equatable, Hashable, Codable {
+        
+        public let microLamports: UInt64
+        
+        public init(microLamports: UInt64) {
+            self.microLamports = microLamports
+        }
+    }
+}
+
+// MARK: - InstructionType -
+
+extension ComputeBudgetProgram.SetComputeUnitPrice: InstructionType {
+    
+    public init(instruction: Instruction) throws {
+        var data   = try ComputeBudgetProgram.parse(.setComputeUnitPrice, instruction: instruction, expectingAccounts: 0)
+        let stride = MemoryLayout<UInt64>.stride
+        
+        guard data.canConsume(stride), let microLamports = UInt64(data: data.consume(stride)) else {
+            throw ErrorGeneric.unknown
+        }
+        
+        self.init(microLamports: microLamports)
+    }
+    
+    public func instruction() -> Instruction {
+        Instruction(
+            program: SystemProgram.address,
+            accounts: [],
+            data: encode()
+        )
+    }
+    
+    public func encode() -> Data {
+        var data = Data()
+        
+        data.append(contentsOf: ComputeBudgetProgram.Command.setComputeUnitPrice.rawValue.bytes)
+        data.append(contentsOf: microLamports.bytes)
+        
+        return data
+    }
+}
