c60181b9f9ab3831241856ca6a05fdb761572745
Dima Bart <dima.bart01@gmail.com>
2024-10-25 16:28:26 -0400

    Add FlipchatServices tests.

---
+++ FlipchatServices/Tests/FlipchatServicesTests/FlipchatTests.swift
@@ -0,0 +1,16 @@
+//
+//  FlipchatServicesTests.swift
+//  FlipchatServicesTests
+//
+//  Created by Dima Bart on 2024-09-24.
+//
+
+import Testing
+@testable import FlipchatServices
+
+struct FlipchatTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+}
