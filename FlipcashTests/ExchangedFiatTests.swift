c6641e524ab43c5f109a7f4c0a9cec52d23bc787
Dima Bart <dima.bart01@gmail.com>
2025-11-06 15:23:48 -0500

    Add fiat validation tests.

--- FlipcashTests/ExchangedFiatTests.swift
+++ FlipcashTests/ExchangedFiatTests.swift
@@ -148,4 +148,145 @@ struct ExchangedFiatTests {
         #expect(cad.converted.quarks          == 5899999999)       // $0.59 CAD
         #expect(cad.rate.fx.formatted(to: 10) == "0.0139997412")
     }
+    
+    // MARK: - Validating Fiat Values -
+    
+    @Test
+    func testAmountsToSend() throws {
+        let startSupply =          1_00_00_000_000 as UInt64 // USDC quarks (6dp)
+        let endSupply   = 21_000_000_00_00_000_000 as UInt64 // 100,000
+        
+        let fiatToTest: [Decimal] = [
+            5.00,
+            10.00,
+            100.00,
+            500.00,
+            1_000.00,
+        ]
+        
+        var output = ""
+
+        var supply = startSupply
+        while supply <= endSupply {
+            
+            for fiat in fiatToTest {
+                let exchanged = ExchangedFiat.computeFromEntered(
+                    amount: fiat,
+                    rate: .oneToOne,
+                    mint: .usdcAuthority, // Not USDC
+                    supplyFromBonding: supply
+                )
+                
+                let supplyStr  = "\(supply)".padded(to: 20)
+                let underlying = "\(exchanged!.usdc.quarks)".padded(to: 20)
+                let converted  = exchanged!.converted.formatted().padded(to: 20)
+                
+                output.append("\(supplyStr) \(underlying) \(converted)\n")
+            }
+            
+            supply *= 10
+        }
+        
+        output = output.trimmingCharacters(in: .newlines)
+        
+        let expected = """
+        10000000000          5001092401997        $5.00               
+        10000000000          10004379663394       $10.00              
+        10000000000          100441080923772      $100.00             
+        10000000000          511295760602584      $500.00             
+        10000000000          1046604099690267     $1,000.00           
+        100000000000         5001052911980        $5.00               
+        100000000000         10004300648691       $10.00              
+        100000000000         100440284483693      $100.00             
+        100000000000         511291632270217      $500.00             
+        100000000000         1046595446081380     $1,000.00           
+        1000000000000        5000658028967        $5.00               
+        1000000000000        10003510535997       $10.00              
+        1000000000000        100432320431767      $100.00             
+        1000000000000        511250350821238      $500.00             
+        1000000000000        1046508914111069     $1,000.00           
+        10000000000000       4996710913709        $5.00               
+        10000000000000       9995612841801        $10.00              
+        10000000000000       100352714788750      $100.00             
+        10000000000000       510837723741345      $500.00             
+        10000000000000       1045644006120180     $1,000.00           
+        100000000000000      4957410747532        $5.00               
+        100000000000000      9916978171984        $10.00              
+        100000000000000      99560135637918       $100.00             
+        100000000000000      506730134317242      $500.00             
+        100000000000000      1037035954468485     $1,000.00           
+        1000000000000000     4581018238122        $5.00               
+        1000000000000000     9163878032451        $10.00              
+        1000000000000000     91971957711638       $100.00             
+        1000000000000000     467464270426932      $500.00             
+        1000000000000000     954919469210810      $1,000.00           
+        10000000000000000    2079970815228        $5.00               
+        10000000000000000    4160321190168        $10.00              
+        10000000000000000    41671690967489       $100.00             
+        10000000000000000    209898607695880      $500.00             
+        10000000000000000    423734428251854      $1,000.00           
+        100000000000000000   775257916            $5.00               
+        100000000000000000   1550515885           $10.00              
+        100000000000000000   15505168342          $100.00             
+        100000000000000000   77526052595          $500.00             
+        100000000000000000   155052632401         $1,000.00           
+        """
+        
+        print(output)
+        #expect(output == expected)
+    }
+    
+    @Test
+    func testQuarksToBalanceConversion() throws {
+        let startTVL =           1_000_000 as UInt64 // USDC quarks (6dp)
+        let endTVL   = 100_000_000_000_000 as UInt64 // 100,000
+        
+        let quarks = 1000_000_000_000 as UInt64 // 100 tokens
+        
+        var output = ""
+
+        var index = 0
+        var tvl = startTVL
+        while tvl <= endTVL {
+            let exchanged = ExchangedFiat.computeFromQuarks(
+                quarks: quarks,//quarksToTest[index],
+                mint: .usdcAuthority,
+                rate: .oneToOne,
+                tvl: tvl
+            )
+            
+            let tvlStr     = "\(tvl)".padded(to: 20)
+            let underlying = "\(exchanged.usdc.quarks)".padded(to: 20)
+            let converted  = exchanged.converted.formatted().padded(to: 20)
+            
+            tvl *= 10
+            index += 1
+            
+            output.append("\(tvlStr) \(underlying) \(converted)\n")
+        }
+        
+        output = output.trimmingCharacters(in: .newlines)
+        
+        let expected = """
+        1000000              1000000000000        $1.00               
+        10000000             1000000000000        $1.00               
+        100000000            1000000000000        $1.01               
+        1000000000           1000000000000        $1.09               
+        10000000000          1000000000000        $1.88               
+        100000000000         1000000000000        $9.77               
+        1000000000000        1000000000000        $88.71              
+        10000000000000       1000000000000        $878.14             
+        100000000000000      1000000000000        $8,772.37           
+        """
+        
+        print(output)
+        #expect(output == expected)
+    }
+}
+
+private extension String {
+    func padded(to characters: Int) -> String {
+        let paddingCount = max(0, characters - self.count)
+        return self + String(repeating: " ", count: paddingCount)
+    }
 }
