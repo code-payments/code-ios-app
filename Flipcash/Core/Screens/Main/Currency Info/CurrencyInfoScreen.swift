b89952fcca8982e6c9589e91eddfee2820bb015f
Raul Riera <rieraraul@gmail.com>
2026-02-05 09:49:37 -0500

    chore: bring the changes from the base branch into this refactor

--- Flipcash/Core/Screens/Main/Currency Info/CurrencyInfoScreen.swift
+++ Flipcash/Core/Screens/Main/Currency Info/CurrencyInfoScreen.swift
@@ -249,7 +249,7 @@ struct CurrencyInfoScreen: View {
             // Floating Footer
             if !isUSDF {
                 CurrencyInfoFooter {
-                    CodeButton(style: .filledAlternative, title: "Buy") {
+                    CodeButton(style: .filled, title: "Buy") {
                         isShowingFundingSelection = true
                     }
 
