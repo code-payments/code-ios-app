926eaebde64b8ebb3cc43cf87e80d72b6d9bb583
Raul Riera <rieraraul@gmail.com>
2026-02-06 15:56:35 -0500

    fix: give auto currency selection

---
+++ FlipcashTests/TestSupport/PublicKey+TestSupport.swift
@@ -0,0 +1,12 @@
+//
+//  PublicKey+TestSupport.swift
+//  Code
+//
+//  Created by Raul Riera on 2026-02-06.
+//
+
+import FlipcashCore
+
+extension PublicKey {
+    static let jeffy = try! PublicKey(base58: "54ggcQ23uen5b9QXMAns99MQNTKn7iyzq4wvCW6e8r25")
+}
