5532f0f6a14ee04b5a6e11a0bff68e486b2a0633
Raul Riera <rieraraul@gmail.com>
2026-02-09 15:58:31 -0500

    fix: update the colors of the access key (#76)

--- FlipcashUI/Sources/FlipcashUI/Views/AccessKey.swift
+++ FlipcashUI/Sources/FlipcashUI/Views/AccessKey.swift
@@ -32,79 +32,79 @@ public struct AccessKey: View {
     
     public var body: some View {
         Rectangle()
-            .fill(Color.clear)
+            .fill(Color.black)
             .aspectRatio(0.607, contentMode: .fit)
-            .background(
-                LinearGradient(
-                    gradient: Gradient(stops: [
-                        Gradient.Stop(color: Color(r: 4,  g: 32, b: 5),  location: 0.0),
-                        Gradient.Stop(color: Color(r: 12, g: 41, b: 26), location: 0.57),
-                        Gradient.Stop(color: Color(r: 0,  g: 70, b: 2),  location: 0.65),
-                        Gradient.Stop(color: Color(r: 0,  g: 26, b: 12), location: 1.00),
-                    ]),
-                    startPoint: UnitPoint(x: -0.5, y: 1.0),
-                    endPoint:   UnitPoint(x: 1.35,  y: 0.15)
-                )
-                .scaleEffect(1.3)
-                .overlay {
-                    RadialGradient(
-                        gradient: Gradient(colors: [
-                            Color.white.opacity(0.2),
-                            Color.white.opacity(0)
-                        ]),
-                        center: .topLeading,
-                        startRadius: 0,
-                        endRadius: 200
+            .overlay(
+                Rectangle()
+                    .foregroundColor(.clear)
+                    .background(
+                        EllipticalGradient(
+                            stops: [
+                                Gradient.Stop(color: .white, location: 0.00),
+                                Gradient.Stop(color: .white.opacity(0), location: 1.00),
+                            ],
+                            center: UnitPoint(x: 0, y: -0.09)
+                        )
                     )
-                }
-                .blur(radius: 20)
+                    .background(
+                        LinearGradient(
+                            stops: [
+                                Gradient.Stop(color: Color(red: 0.22, green: 0.22, blue: 0.26), location: 0.17),
+                                Gradient.Stop(color: Color(red: 0.21, green: 0.21, blue: 0.22), location: 0.60),
+                                Gradient.Stop(color: Color(red: 0.76, green: 0.76, blue: 0.76), location: 0.81),
+                                Gradient.Stop(color: Color(red: 0.1, green: 0.1, blue: 0.1), location: 1.00),
+                            ],
+                            startPoint: UnitPoint(x: 0.2, y: 0.91),
+                            endPoint: UnitPoint(x: 0.81, y: 0.11)
+                        )
+                    )
+                    .blur(radius: 31)
+                    .opacity(0.29)
             )
             .overlay(
-                ZStack {
-                    VStack {
-                        Image.asset(.flipcashLogo)
-                            .renderingMode(.template)
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .frame(maxHeight: 50)
-                            .foregroundStyle(Color.white)
-                            
-                        Spacer()
-                        
-                        QRCode(
-                            string: url.absoluteString,
-                            showLabel: false,
-                            codeColor: .white,
-                            backgroundColor: .clear
-                        )
-                        .aspectRatio(1, contentMode: .fit)
-                        .frame(maxWidth: 145)
-                        
-                        Spacer()
-                        
-                        VStack {
-                            ForEach(mnemonicGroups, id: \.self) { group in
-                                Text(group)
-                            }
-                        }
-                        .font(.appTextAccessKey)
-                    }
-                    .padding([.top, .bottom], 60)
-                    
-                    RadialGradient(
-                        gradient: Gradient(stops: [
-                            Gradient.Stop(color: Color(r: 255, g: 255, b: 255, o: 0.1), location: 0.0),
-                            Gradient.Stop(color: Color(r: 255, g: 255, b: 255, o: 0),   location: 1.0),
-                        ]),
-                        center: UnitPoint(x: 0.0, y: 0.0),
-                        startRadius: 0,
-                        endRadius: 300
+                VStack {
+                    Image.asset(.flipcashLogo)
+                        .renderingMode(.template)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(maxHeight: 50)
+                        .foregroundStyle(Color.white)
+
+                    Spacer()
+
+                    QRCode(
+                        string: url.absoluteString,
+                        showLabel: false,
+                        codeColor: .white,
+                        backgroundColor: .clear
                     )
+                    .aspectRatio(1, contentMode: .fit)
+                    .frame(maxWidth: 145)
+
+                    Spacer()
+
+                    VStack {
+                        ForEach(mnemonicGroups, id: \.self) { group in
+                            Text(group)
+                        }
+                    }
+                    .font(.appTextAccessKey)
                 }
+                .padding([.top, .bottom], 60)
             )
             .clipped()
             .cornerRadius(10)
             .drawingGroup()
             .frame(maxWidth: 255)
     }
+}
+
+#Preview {
+    Color.black
+        .overlay {
+            AccessKey(mnemonic:
+                        MnemonicPhrase(words: ["pill", "tomorrow", "foster", "begin", "walnut", "borrow", "virtual", "kick", "shift", "mutual", "shoe", "scatter"])!,
+                      url: URL(string: "https://flipcash.com")!
+            )
+        }
 }
