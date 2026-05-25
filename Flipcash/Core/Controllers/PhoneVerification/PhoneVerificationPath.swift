4e5e77b35a74a2e25079209b60ec9baa4a0c5a75
Raul Riera <rieraraul@gmail.com>
2026-05-25 15:59:48 -0400

    refactor: move phone screens to PhoneVerification folder
    File moves only. Adds PhoneVerificationPath enum.
    No behavior change.

---
+++ Flipcash/Core/Controllers/PhoneVerification/PhoneVerificationPath.swift
@@ -0,0 +1,10 @@
+//
+//  PhoneVerificationPath.swift
+//  Flipcash
+//
+
+/// Navigation path for a standalone phone verification flow's NavigationStack.
+enum PhoneVerificationPath: Hashable {
+    case enterPhoneNumber
+    case confirmPhoneNumberCode
+}
