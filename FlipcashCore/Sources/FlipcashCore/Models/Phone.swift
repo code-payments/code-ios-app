8f7fba769f6ad6b12951433bea695c2e7cc6f1f8
Raul Riera <rieraraul@gmail.com>
2026-05-25 15:33:17 -0400

    chore: remove dead code from Phone.swift (#314)
    Removes five unused symbols:
    - Phone.unicodeFlag (+ private region/unicodeFlagFor helpers)
    - PhoneDescription struct + typo'd RegistraionStatus enum
    - Phone.mock test extension

--- FlipcashCore/Sources/FlipcashCore/Models/Phone.swift
+++ FlipcashCore/Sources/FlipcashCore/Models/Phone.swift
@@ -11,47 +11,20 @@ import Foundation
 @preconcurrency import PhoneNumberKit
 
 public struct Phone: Codable, Equatable, Hashable, Sendable {
-    
+
     public let e164: String
     public let national: String
-    
+
     private let phoneNumber: PhoneNumber
-    
-    public var unicodeFlag: String? {
-        if let region {
-            return Self.unicodeFlagFor(region: region.rawValue)
-        }
-        return nil
-    }
-    
-    private var region: Region? {
-        if let region = phoneNumber.regionID {
-            return Region(regionCode: region)!
-        }
-        return nil
-    }
-    
-    // MARK: - Init -
-    
+
     public init?(_ string: String) {
         guard let phoneNumber = try? Self.phoneNumberUtility.parse(string) else {
             return nil
         }
-        
+
         self.phoneNumber = phoneNumber
         self.e164        = Self.phoneNumberUtility.format(phoneNumber, toType: PhoneNumberFormat.e164)
         self.national    = Self.phoneNumberUtility.format(phoneNumber, toType: PhoneNumberFormat.national)
-    }
-    
-    // MARK: - Flag -
-    
-    private static func unicodeFlagFor(region: String) -> String {
-        let flagBase: UInt32 = 0x0001F1A5
-        return region
-            .uppercased()
-            .unicodeScalars
-            .compactMap { UnicodeScalar(flagBase + $0.value)?.description }
-            .joined()
     }
 }
 
@@ -82,32 +55,4 @@ public struct PhoneFormatter {
     public func format(_ rawPhoneNumber: String) -> String {
         partialFormatter.formatPartial(rawPhoneNumber)
     }
-}
-
-// MARK: - PhoneDescription -
-
-public struct PhoneDescription: Hashable {
-    
-    public let phone: Phone
-    public let status: RegistraionStatus
-    
-    init(phone: Phone, status: RegistraionStatus) {
-        self.phone = phone
-        self.status = status
-    }
-}
-
-extension PhoneDescription {
-    public enum RegistraionStatus: Hashable {
-        case uploaded
-        case registered
-        case invited
-        case revoked
-    }
-}
-
-// MARK: - Mock -
-
-extension Phone {
-    public static let mock = Phone("+16472222222")!
 }
