6990f50e6b3ae4d7d9c433044492047ee6d6b35f
Raul Riera <rieraraul@gmail.com>
2026-05-26 10:33:25 -0400

    fix: remove conflict markers left by rebase

--- FlipcashTests/TestSupport/Phone+TestSupport.swift
+++ FlipcashTests/TestSupport/Phone+TestSupport.swift
@@ -3,16 +3,9 @@
 //  FlipcashTests
 //
 
-<<<<<<< HEAD
 import Foundation
 import FlipcashCore
 
 extension Phone {
-
-=======
-import FlipcashCore
-
-extension Phone {
->>>>>>> b8c902ac (refactor: extract PhoneVerificationViewModel; wrap from OnrampVerificationViewModel)
     static let mock = Phone("+16472222222")!
 }
