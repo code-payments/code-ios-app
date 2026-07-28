cac9321d6942d152c8dee81435fdef197d45a4bd
Brandon McAnsh <git@bmcreations.dev>
2026-07-27 20:40:55 -0400

    fix(tests): support Xcode 27 / Swift 6.4 local test runs (#528)

--- CodeScanner/CodeScanner/Code.mm
+++ CodeScanner/CodeScanner/Code.mm
@@ -20,33 +20,39 @@
 
 @implementation KikCodes
 
+// zxing's Reed-Solomon tables and Counted refcounts are not thread-safe; serialize every entry point.
+
 + (nonnull NSData *)encode:(nonnull NSData *)data {
-    unsigned char outData[MAIN_BYTE_COUNT] = ZERO_BYTES;
-    
-    uint8_t bytes[PAYLOAD_BYTE_COUNT] = ZERO_BYTES;
-    memcpy(bytes, data.bytes, data.length);
-    
-    kikCodeEncodeRemote(outData, (unsigned char *)bytes, 0);
+    @synchronized (self) {
+        unsigned char outData[MAIN_BYTE_COUNT] = ZERO_BYTES;
+
+        uint8_t bytes[PAYLOAD_BYTE_COUNT] = ZERO_BYTES;
+        memcpy(bytes, data.bytes, data.length);
+
+        kikCodeEncodeRemote(outData, (unsigned char *)bytes, 0);
 //    kikCodeEncodeGroup(outData, (unsigned char *)data.bytes, 0);
-    
-    return [[NSData alloc] initWithBytes:outData length:MAIN_BYTE_COUNT];
+
+        return [[NSData alloc] initWithBytes:outData length:MAIN_BYTE_COUNT];
+    }
 }
 
 + (nonnull NSData *)decode:(nonnull NSData *)data {
-    KikCodePayload payload;
-    unsigned int type;
-    unsigned int color;
-    kikCodeDecode((unsigned char *)data.bytes, &type, &payload, &color);
-    
-    // Trim any tail zero bytes at the tail
-    
-    uint8_t *bytes = payload.group.invite_code;
-    int length = sizeof(payload.group.invite_code);
-    while (length > 0 && bytes[length - 1] == 0x0) {
-        length--;
+    @synchronized (self) {
+        KikCodePayload payload;
+        unsigned int type;
+        unsigned int color;
+        kikCodeDecode((unsigned char *)data.bytes, &type, &payload, &color);
+
+        // Trim any tail zero bytes at the tail
+
+        uint8_t *bytes = payload.group.invite_code;
+        int length = sizeof(payload.group.invite_code);
+        while (length > 0 && bytes[length - 1] == 0x0) {
+            length--;
+        }
+
+        return [[NSData alloc] initWithBytes:bytes length:length];
     }
-    
-    return [[NSData alloc] initWithBytes:bytes length:length];
 }
 
 + (nullable NSData *)scan:(nonnull NSData *)data width:(NSInteger)width height:(NSInteger)height {
@@ -54,15 +60,17 @@
 }
 
 + (nullable NSData *)scan:(nonnull NSData *)data width:(NSInteger)width height:(NSInteger)height quality:(KikCodesScanQuality)quality {
-    uint8_t outData[MAIN_BYTE_COUNT] = ZERO_BYTES;
-    
-    unsigned int qualityValue = [self deviceQualityForScanQuality:quality];
-    
-    int result = kikCodeScan((unsigned char *)data.bytes, (unsigned int)width, (unsigned int)height, qualityValue, outData, nil, nil, nil, nil);
-    if (result == 0) {
-        return [[NSData alloc] initWithBytes:outData length:MAIN_BYTE_COUNT];
+    @synchronized (self) {
+        uint8_t outData[MAIN_BYTE_COUNT] = ZERO_BYTES;
+
+        unsigned int qualityValue = [self deviceQualityForScanQuality:quality];
+
+        int result = kikCodeScan((unsigned char *)data.bytes, (unsigned int)width, (unsigned int)height, qualityValue, outData, nil, nil, nil, nil);
+        if (result == 0) {
+            return [[NSData alloc] initWithBytes:outData length:MAIN_BYTE_COUNT];
+        }
+        return nil;
     }
-    return nil;
 }
 
 + (int)deviceQualityForScanQuality:(KikCodesScanQuality)quality {
